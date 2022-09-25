21892cfc985f1d0499b5bf5596e5cbc3c1865c11
jasfer <47032082+xtrnals@users.noreply.github.com>
2022-09-25 15:37:17 +0400

    2

---
+++ Chapter_1_Exercise_2.py
@@ -0,0 +1,62 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyMANpDa4FDwgVYDJDPebZqG",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/xtrnals/Chapter-1/blob/main/Chapter_1_Exercise_2.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "markdown",
+      "source": [
+        "Exercise 2: Print the Version of Python ☑️"
+      ],
+      "metadata": {
+        "id": "gNK5YM6kXruJ"
+      }
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "UCRPz0y6XiIK",
+        "outputId": "4d92f4e1-7005-45fc-8357-a814518e7189"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "Python 3.7.14\n"
+          ]
+        }
+      ],
+      "source": [
+        "!python --version"
+      ]
+    }
+  ]
+}
