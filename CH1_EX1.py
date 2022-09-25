b1f4ed64d71267af7d359ed76006c501adcde393
jasfer <47032082+xtrnals@users.noreply.github.com>
2022-09-25 15:29:16 +0400

    Created using Colaboratory

---
+++ CH1_EX1.py
@@ -0,0 +1,63 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyPnz3E4DEj079wq2QOc7oiY",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/xtrnals/Chapter-1/blob/main/CH1_EX1.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "markdown",
+      "source": [
+        "Exercise 1: Print Strings☑️"
+      ],
+      "metadata": {
+        "id": "guMDcR-OXaKK"
+      }
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "print (\"Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I \\nwonder what you are\")"
+      ],
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "a5bTUyHSXcgJ",
+        "outputId": "c2ccf0a9-09dc-432f-9137-a7e769d92cbe"
+      },
+      "execution_count": null,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I \n",
+            "wonder what you are\n"
+          ]
+        }
+      ]
+    }
+  ]
+}
